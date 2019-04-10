b775a090d7a857e215e5446ee90e3a6dbe857436
EarthScientist <lindgren.mike@gmail.com>
2019-04-09 16:57:23 -0800

    update environment variables

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from collections import defaultdict
 # #  SERDP-FISH-FIRE Data Explorer Tool                                 # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-mapbox_access_token = os.environ['MAPBOX_ACCESS_TOKEN']
-
+# mapbox_access_token = os.environ['MAPBOX_ACCESS_TOKEN']
+mapbox_access_token = 'pk.eyJ1IjoiZWFydGhzY2llbnRpc3QiLCJhIjoiY2o4b3J5eXdwMDZ5eDM4cXU4dzJsMGIyZiJ9.a5IlzVUBGzJbQ0ayHC6t1w'
 print(mapbox_access_token)
 
 def load_data():
